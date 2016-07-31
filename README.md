09404d513bf9e8fa8517a4e9c585fcc6aa57f4d7
John Myles White <jmw@johnmyleswhite.com>
2016-07-31 18:15:05 -0500

    Basic filter and summarize implementations

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-A demo of eager evaluation of arbitrary functions over tabular data.
+ A demo of eager evaluation of arbitrary functions over tabular data.
 
 ```
-import TBL: @mutate
+import TBL: @mutate, @filter, @summarize
 import NullableArrays: NullableArray
-import Distributions
+import Distributions: Normal, pdf
 
 tbl = Dict(
     :a => convert(NullableArray, [1, 2, 3]),
@@ -11,11 +11,38 @@ tbl = Dict(
     :c => convert(NullableArray, [7.0, 8.0, 9.0]),
 )
 
-@mutate(
+tbl2 = @mutate(
     tbl,
     d = a + b * sqrt(c),
     e = digamma(d)^2
 )
+
+tbl3 = @filter(tbl2, d > 15)
+
+tbl4 = @summarize(tbl3, m = mean(e), s = std(e))
+
+@summarize(tbl, m = sum(a + b * c))
+
+@mutate(
+    tbl,
+    d = a + rand(),
+    e = digamma(d)^2
+)
+
+function foo(tbl, x)
+    @mutate(tbl, d = a + rand() + :x)
+end
+foo(tbl, 1)
+
+x = 1
+@filter(tbl, a == :x)
+
+@filter(tbl, a > :x)
+
+@filter(tbl, a == 1)
+@filter(tbl, a > 1)
+@filter(tbl, b == 5)
+@filter(tbl, a == 1 || b == 5)
 ```
 
 At the moment, any expression that does not have a concrete return type
@@ -25,6 +52,6 @@ throws an error. So the following example won't work yet:
 @mutate(
     tbl,
     d = a + b * sqrt(c),
-    e = quantile(Normal(0, 1), a)
+    e = pdf(Normal(0, 1), c)
 )
 ```
--- src/03_apply.jl
+++ src/03_apply.jl
@@ -19,11 +19,18 @@ into which to store the results.
     # See if inference knows the return type for the tuple-to-scalar function.
     t = Core.Inference.return_type(f, (Tuple{inner_types...}, ))
 
-    # Branch here on non-concrete types.
+    # If there is no method found (or it returns nothing), t === Union{}.
+    # This happens if you invoke stuff not in the shared Base scope.
+    # if t == Union{}
+    #     error("No method found for those types")
+    # end
+
+    # Branch here on non-concrete types on a semantically correct path.
     if !isleaftype(t)
         # TODO: Make this a real code path based on the type-inference
         # independent map implementation in Base.
-        error("Unable to process type-inference-unfriendly functions")
+        @printf("WARNING: Failed to type-infer expression: found %s", t)
+        t = Any
     end
 
     # Pre-allocate the table's new column.
@@ -35,4 +42,64 @@ into which to store the results.
 
     # Return the output
     return output
+end
+
+"""
+Boolean tuple func checker
+"""
+@noinline function _boolean_tuple_func(f, tbl, colnames)
+    # Extract the columns from the table and create a tuple iterator.
+    cols = [tbl[colname] for colname in colnames]
+    tpl_itr = zip(cols...)
+
+    # Pre-allocate the table's new column.
+    n = length(tbl[colnames[1]])
+    indices = Array(Int, 0)
+
+    # Fill the new column in row-by-row.
+    _grow_output!(indices, f, tpl_itr)
+
+    # Return the output
+    return indices
+end
+
+"""
+Summarize tuple function
+"""
+@noinline function _summarize_tuple_func(f, g, tbl, colnames)
+    # Extract the columns from the table and create a tuple iterator.
+    cols = [tbl[colname] for colname in colnames]
+    tpl_itr = zip(cols...)
+
+    # Determine the tuple type of the iterator after unwrapping all Nullables.
+    #
+    # TODO: Handle the possibility of columns that are not NullableArrays here.
+    # Better yet, don't handle that possibility and avoid a lot of shanigans
+    # elsewhere.
+    inner_types = map(x -> eltype(eltype(x)), cols)
+
+    # See if inference knows the return type for the tuple-to-scalar function.
+    t = Core.Inference.return_type(f, (Tuple{inner_types...}, ))
+
+    # If there is no method found (or it returns nothing), t === Union{}.
+    # if t == Union{}
+    #     error("No method found for those types")
+    # end
+
+    # Branch here on non-concrete types.
+    if !isleaftype(t)
+        # TODO: Make this a real code path based on the type-inference
+        # independent map implementation in Base.
+        @printf("WARNING: Failed to type-infer expression: found %s", t)
+        t = Any
+    end
+
+    # Allocate a temporary column.
+    temporary = Array(t, 0)
+
+    # Fill the new column in row-by-row, skipping nulls.
+    _grow_nonnull_output!(temporary, f, tpl_itr)
+
+    # Return the summarization function applied to the temporary.
+    return g(temporary)
 end
